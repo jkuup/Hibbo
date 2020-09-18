f70d0ea570550e6f7b683af153d71a41a1c6f5f5
jkuup <alphkiee@gmail.com>
2020-09-18 10:34:16 +0800

    comment

--- utils/cmd.go
+++ utils/cmd.go
@@ -4,9 +4,13 @@ import (
 	"os/exec"
 )
 
+// 在指定的目录下运行命令
 func RunCmdByDir(dir string, cmdName string, arg ...string) (string, error) {
-	cmd := exec.Command(cmdName, arg ...)
+	// exec.Command 返回一个新的Cmd
+	cmd := exec.Command(cmdName, arg...)
+	// 命令运行的目录
 	cmd.Dir = dir
+	// 标准输出和错误输出合并
 	out, err := cmd.CombinedOutput()
 	if err != nil {
 		return "", err
