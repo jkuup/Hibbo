29a6c6a1361a4214417ae61d530cf1ce12dedbf2
jkuup <alphkiee@gmail.com>
2020-09-20 15:01:48 +0800

    model

---
+++ config/common.go
@@ -0,0 +1,103 @@
+package config
+
+import (
+	"encoding/json"
+	"errors"
+	"io/ioutil"
+	"log"
+	"os"
+	"os/exec"
+	"pibbo/utils"
+	"strings"
+)
+
+type Config struct {
+	userConfig
+	systemConfig
+}
+
+var Cfg Config
+
+func init() {
+	var err error
+
+	Cfg.CurrentDir, err = os.Getwd()
+	if err != nil {
+		panic(err)
+	}
+
+	configFile, err := ioutil.ReadFile(Cfg.CurrentDir + "/config.json")
+	if err != nil {
+		panic(err)
+	}
+	// 读取配置文件到Cfg
+	err = json.Unmarshal(configFile, &Cfg)
+	if err != nil {
+		panic(err)
+	}
+
+	// dashboard的访问路径的判断
+	if "" == Cfg.DashboardEntrance ||
+		!strings.HasPrefix(Cfg.DashboardEntrance, "/") {
+		Cfg.DashboardEntrance = "/admin"
+	}
+
+	repoName, err := utils.GetRepoName(Cfg.DocumentGitUrl)
+
+	if err != nil {
+		panic(err)
+	}
+
+	Cfg.AppName = "Github"
+	Cfg.Version = 0.1
+	Cfg.DocumentDir = Cfg.CurrentDir + "/" + repoName
+	Cfg.GitHookUrl = "/api/git_push_hook"
+	Cfg.AppRepository = "https://github.com/jkuup/"
+}
+
+func Initial() {
+	if _, err := exec.LookPath("git"); err != nil {
+		log.Println("请先安装git")
+		panic(err)
+	}
+	if !utils.IsDir(Cfg.DocumentDir) {
+		log.Println("正在克隆文档仓库，请稍等...")
+		out, err := utils.RunCmdByDir(Cfg.CurrentDir, "git", "clone", Cfg.DocumentGitUrl)
+		if err != nil {
+			panic(err)
+		}
+		log.Println(out)
+	} else {
+		out, err := utils.RunCmdByDir(Cfg.DocumentDir, "git", "pull")
+		log.Println(out)
+		if err != nil {
+			panic(err)
+		}
+
+	}
+	if err := checkDocDirAndBindConfig(&Cfg); err != nil {
+		log.Println("文档缺少必要的目录")
+		panic(err)
+	}
+
+	imgDir := Cfg.CurrentDir + "/images"
+	if !utils.IsDir(imgDir) {
+		if os.Mkdir(imgDir, os.ModePerm) != nil {
+			panic("生成images目录失败！")
+		}
+	}
+}
+
+func checkDocDirAndBindConfig(cfg *Config) error {
+	dirs := []string{"assets", "content", "extra_nav"}
+	for _, dir := range dirs {
+		absoluteDir := Cfg.DocumentDir + "/" + dir
+		if !utils.IsDir(absoluteDir) {
+			return errors.New("documents cannot lack " + absoluteDir + " dir")
+		}
+	}
+	cfg.DocumentAssetsDir = cfg.DocumentDir + "/assets"
+	cfg.DocumentContentDir = cfg.DocumentDir + "/content"
+	cfg.DocumentExtraNavDir = cfg.DocumentDir + "/extra_nav"
+	return nil
+}
