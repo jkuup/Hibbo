1e9260126b978d02616c6493080d21378c818390
jkuup <alphkiee@gmail.com>
2020-09-23 13:40:10 +0800

    controller

---
+++ controller/category.go
@@ -0,0 +1,24 @@
+package controller
+
+import (
+	"net/http"
+	"pibbo/config"
+	"pibbo/models"
+
+	"github.com/gin-gonic/gin"
+)
+
+func Category(c *gin.Context) {
+	//categoriesTemplate := models.Template.Categories
+
+	result := models.GroupByCategory(&models.ArticleList, config.Cfg.CategoryDisplayQuantity)
+
+	//categoriesTemplate.WriteData(c.Writer, models.BuildViewData("Blog", result))
+
+	c.HTML(http.StatusOK, "categories.html", gin.H{
+		"Title":  "Blog",
+		"Data":   result,
+		"Config": config.Cfg,
+		"Navs":   models.Navigation,
+	})
+}
